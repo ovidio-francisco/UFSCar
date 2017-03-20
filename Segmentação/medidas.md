e0c636598c6303bfac20ff3b8a486c2a1c302031
ovidio-francisco <ovidiojf@gmail.com>
2017-03-20 09:12:59 -0300

    update

---
+++ Segmentação/medidas.md
@@ -0,0 +1,4 @@
+
+# Medidas de Avaliação
+
+
--- codes/TextSegmenterBR /src/userInterface/Main.java
+++ codes/TextSegmenterBR /src/userInterface/Main.java
@@ -23,55 +23,9 @@ public class Main {
 //		
 //		
 //		System.out.println(String.format("[%s]", TextUtils.restrictChar(s, ' ', 2)));
-		
-		String txt = "O samba é um gênero musical, de raízes africanas, surgido no Brasil e considerado uma das principais manifestações culturais populares brasileiras.";
 
-		
-		String[] ss = txt.split(" ");
-		
-		
-		StopWords sw = new StopWordList(new File("stopPort.txt"));
-		
-		
-		for(String s : ss) {
-			System.out.println(s + " - " +sw.isStopWord(s));
-//			System.out.println(s);
-		}
-		System.out.println("\n\n=============\n\n");
 
-		for(String s : ss) {
-			if (!sw.isStopWord(s)) 
-					System.out.println(s);
-		}
-		
-		System.out.println("\n\n=============\n\n");
-		
-		
-//		for(String stopw : sw.getStopWords()) {
-//			System.out.println(stopw);
-//		}
-		
-		ptstemmer.Stemmer stt = ptstemmer.Stemmer.StemmerFactory(StemmerType.ORENGO);
-		
-		Stemmer st = new Stemmer() {
-			
-			@Override
-			public String stem(String word) {
-				return null;
-			}
-		};
-		
-		
-		
-		for(String s : ss) {
-			
-			String sss = Cleaner.removePunctuation(s);
-			
-			if (!sw.isStopWord(sss)) 
-					System.out.println(stt.wordStemming(sss));
-		}
-		
-		System.out.println("\n\n=============\n\n");
+		StopWordList.showExample();
 		
 	}
 	
