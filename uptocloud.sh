52e030b0b313e02452914f694df667ace27119d9
ovidio-francisco <ovidiojf@gmail.com>
2017-07-20 16:00:48 -0300

    update

--- uptocloud.sh
+++ uptocloud.sh
@@ -65,7 +65,7 @@ echo ""
 status=$(git status)
 
 echo "status:"
-"$status" | grep --color 'up-to-date\|nothing to commit'
+eval "$status | grep --color 'up-to-date\|nothing to commit'"
 
 
 if [[ "$status" == *"up-to-date"* ]] ; then 
